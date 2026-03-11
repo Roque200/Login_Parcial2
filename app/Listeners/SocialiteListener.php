2164a59eb53f2227ac71ef469880e22dbe5c05c7
roberto roque <21031190@itcelaya.edu.mx>
2026-03-11 14:10:54 -0600

    Discord en fucnionamineto

--- app/Listeners/SocialiteListener.php
+++ app/Listeners/SocialiteListener.php
@@ -9,5 +9,6 @@ class SocialiteListener
     public function handle(SocialiteWasCalled $socialiteWasCalled): void
     {
         $socialiteWasCalled->extendSocialite('spotify', \SocialiteProviders\Spotify\Provider::class);
+        $socialiteWasCalled->extendSocialite('discord', \SocialiteProviders\Discord\Provider::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SocialAuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +12,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirect'])
